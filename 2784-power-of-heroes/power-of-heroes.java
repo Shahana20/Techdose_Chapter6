82c7b30db5596e490795cc1b37a82e33248f1201
Shahana20 <107378734+Shahana20@users.noreply.github.com>
2023-09-14 23:04:47 +0530

    Time: 13 ms (89.69%) | Memory: 53.9 MB (97.94%) - LeetSync

---
+++ 2784-power-of-heroes/power-of-heroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int sumOfPower(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        Arrays.sort(nums);
+        long preSum = 0;
+        long res = 0;
+        long mod = 1_000_000_000 + 7;
+        long add = 0;
+        for (int i = 0; i < nums.length; i++) {
+            add = (preSum + nums[i]) * nums[i] % mod * nums[i] % mod;
+            res = (res + add) % mod;
+            preSum = (preSum * 2 + nums[i]) % mod;
+        }
+        return (int) res;
+    }
+}
